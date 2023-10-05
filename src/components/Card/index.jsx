20d34e138d1cbd7b6b9cfa77b9c6d178ac07b416
raiisoares <raisoares.dev@gmail.com>
2023-10-05 05:59:56 -0300

    card authorization

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -7,9 +7,12 @@ import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { api } from "../../services/api";
+import { useAuth } from "../../hooks/auth";
+import { USER_ROLE } from "./../../utils/roles";
 
 export function Card({ data, ...rest }) {
   const navigate = useNavigate();
+  const { user } = useAuth();
   const [price, setPrice] = useState(data.price);
   const [counter, setCounter] = useState(1);
   const [isFavorite, setIsFavorite] = useState(false);
@@ -22,8 +25,10 @@ export function Card({ data, ...rest }) {
     navigate(`/detail/${id}`);
   }
 
-  const handleFavorite = async () => {
-    setIsFavorite(!isFavorite);
+  const handleTopAction = async () => {
+    [USER_ROLE.ADMIN].includes(user.role)
+      ? navigate(`/edit/${data.id}`)
+      : setIsFavorite(!isFavorite);
   };
 
   const handlePlusButton = async () => {
@@ -40,8 +45,26 @@ export function Card({ data, ...rest }) {
 
   return (
     <Container {...rest}>
-      <div className="fav" onClick={handleFavorite}>
-        {isFavorite ? (
+      <div className="fav" onClick={handleTopAction}>
+        {[USER_ROLE.ADMIN].includes(user.role) ? (
+          <ButtonIcon>
+            <svg
+              width="26"
+              height="26"
+              viewBox="0 0 26 26"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M13.7518 4.81056L21.1901 12.2489M8.00046 25.0002H2.06311C1.78129 25.0002 1.51101 24.8883 1.31173 24.689C1.11245 24.4897 1.00049 24.2194 1.00049 23.9376V18.0003C1.00001 17.8623 1.02675 17.7256 1.07917 17.5979C1.13159 17.4703 1.20867 17.3542 1.306 17.2564L17.2452 1.31724C17.3441 1.21684 17.4619 1.1371 17.5919 1.08268C17.7219 1.02826 17.8614 1.00023 18.0023 1.00023C18.1432 1.00023 18.2827 1.02826 18.4127 1.08268C18.5427 1.1371 18.6605 1.21684 18.7594 1.31724L24.6835 7.2413C24.7839 7.34018 24.8636 7.45804 24.918 7.58802C24.9725 7.718 25.0005 7.8575 25.0005 7.99842C25.0005 8.13933 24.9725 8.27883 24.918 8.40881C24.8636 8.53879 24.7839 8.65665 24.6835 8.75553L8.74429 24.6947C8.64648 24.792 8.53043 24.8691 8.4028 24.9216C8.27516 24.974 8.13844 25.0007 8.00046 25.0002Z"
+                stroke="#E1E1E6"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </ButtonIcon>
+        ) : isFavorite ? (
           <ButtonIcon>
             <svg
               width="26"
@@ -86,18 +109,20 @@ export function Card({ data, ...rest }) {
       </button>
       <p>{data.description}</p>
       <span>R$ {price}</span>
-      <div>
-        <div className="actionButtons">
-          <ButtonIcon onClick={handleMinusButton}>
-            <AiOutlineMinus />
-          </ButtonIcon>
-          <span className="counter">{counter}</span>
-          <ButtonIcon onClick={handlePlusButton}>
-            <AiOutlinePlus />
-          </ButtonIcon>
+      {[USER_ROLE.CUSTOMER].includes(user.role) && (
+        <div>
+          <div className="actionButtons">
+            <ButtonIcon onClick={handleMinusButton}>
+              <AiOutlineMinus />
+            </ButtonIcon>
+            <span className="counter">{counter}</span>
+            <ButtonIcon onClick={handlePlusButton}>
+              <AiOutlinePlus />
+            </ButtonIcon>
+          </div>
+          <Button title={"incluir"} />
         </div>
-        <Button title={"incluir"} />
-      </div>
+      )}
     </Container>
   );
 }
